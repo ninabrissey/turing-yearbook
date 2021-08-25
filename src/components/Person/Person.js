4bf6dc8f28eec3968ddd90e08b2fd51e769c3e43
Nina Brissey <nbrissey@gmail.com>
2021-08-25 00:37:32 -0600

    Create Person component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Person.css';
 
 // PERSON COMPONENT CODE GOES HERE
-const Person = () => {};
+const Person = ({ staffMember }) => {
+  const { id, name, quote, superlative, photo } = staffMember;
+  return (
+    <article>
+      <img src={photo} alt={`Photo of ${name}`}></img>
+      <h3>{name}</h3>
+      <p>{quote}</p>
+      <p>{superlative}</p>
+    </article>
+  );
+};
 
 export default Person;
